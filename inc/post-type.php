cc31a1e4094c33d4a3889f1573699a485596c090
Philipp <philipp@ouun.io>
2021-02-10 10:54:18 +0100

    Adds Supporter Sites

--- inc/post-type.php
+++ inc/post-type.php
@@ -24,7 +24,7 @@ if ( ! function_exists( 'repack_supporter_sites_post_type' ) ) {
 	add_action(
 		'init',
 		function () {
-			$labels = array(
+			$labels  = array(
 				'name'                  => _x( 'Supporter Sites', 'Post Type General Name', 'repack-ts' ),
 				'singular_name'         => _x( 'Supporter Site', 'Post Type Singular Name', 'repack-ts' ),
 				'menu_name'             => __( 'Supporter Sites', 'repack-ts' ),
@@ -53,7 +53,13 @@ if ( ! function_exists( 'repack_supporter_sites_post_type' ) ) {
 				'items_list_navigation' => __( 'Sites list navigation', 'repack-ts' ),
 				'filter_items_list'     => __( 'Filter sites list', 'repack-ts' ),
 			);
-			$args   = array(
+			$rewrite = array(
+				'slug'       => 'supporter',
+				'with_front' => true,
+				'pages'      => true,
+				'feeds'      => true,
+			);
+			$args    = array(
 				'label'               => __( 'Supporter Site', 'repack-ts' ),
 				'description'         => __( 'WeRePack Supporter Sites', 'repack-ts' ),
 				'labels'              => $labels,
@@ -67,9 +73,10 @@ if ( ! function_exists( 'repack_supporter_sites_post_type' ) ) {
 				'show_in_admin_bar'   => true,
 				'show_in_nav_menus'   => true,
 				'can_export'          => true,
-				'has_archive'         => 'supporter',
+				'has_archive'         => true,
 				'exclude_from_search' => false,
 				'publicly_queryable'  => true,
+				'rewrite'             => $rewrite,
 				'capability_type'     => 'page',
 				'show_in_rest'        => true,
 			);
